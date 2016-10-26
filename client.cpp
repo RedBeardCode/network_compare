3030f3c1f9d3195f8a93c3150fdfe3d25168e1dc
Stefan Farmbauer <s.farmbauer@red-beard-code.de>
2016-10-26 07:57:42 +0200

    Example with ZeroMq and NanoMsg

--- client.cpp
+++ client.cpp
@@ -1,18 +1,22 @@
 #include <zmq.hpp>
-#include <string>
 #include <iostream>
+#include <nanomsg/nn.h>
+#include <nanomsg/pair.h>
+#include <memory>
 
 int main ()
 {
-    //  Prepare our context and socket
+    size_t  cnum_samples = 5000;
+    size_t cbuf_size = 4000*3000;
+    std::shared_ptr<char> buf(new char[cbuf_size]);
+    //ZeroMq
     zmq::context_t context (1);
     zmq::socket_t socket (context, ZMQ_REQ);
 
     std::cout << "Connecting to hello world server…" << std::endl;
     socket.connect ("tcp://localhost:5555");
 
-    //  Do 10 requests, waiting each time for a response
-    for (int request_nbr = 0; request_nbr != 5000; request_nbr++) {
+    for (int request_nbr = 0; request_nbr != cnum_samples; request_nbr++) {
         zmq::message_t request (5);
         memcpy (request.data (), "Hello", 5);
         std::cout << "Sending Hello " << request_nbr << "…" << std::endl;
@@ -26,5 +30,21 @@ int main ()
     zmq::message_t end(2);
     memcpy(end.data(), "  ", 2);
     socket.send(end);
+
+    //NanoMsg
+
+
+    int sock = nn_socket (AF_SP, NN_PAIR);
+    nn_connect (sock, "tcp://localhost:5556");
+    for (int request_nbr = 0; request_nbr != cnum_samples; request_nbr++) {
+        std::cout << "Sending Hello " << request_nbr << "…" << std::endl;
+        nn_send(sock, "Hello", 5, 0);
+
+        //  Get the reply.
+        int bytes = nn_recv(sock, buf.get(), cbuf_size, 0);
+        std::cout << "Received World " <<bytes << std::endl;
+    }
+    nn_send(sock, "  ", 2, 0);
+    nn_shutdown (sock, 0);
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -8,6 +8,8 @@
 
 #include "sys/times.h"
 
+#include <nanomsg/nn.h>
+#include <nanomsg/pair.h>
 
 static clock_t lastCPU, lastSysCPU, lastUserCPU;
 static int numProcessors;
@@ -61,9 +63,22 @@ double getCurrentValue(){
 bool gMeasure = true;
 std::vector<long double> cpu_samples;
 
+void report_statistics(const size_t cdata_size, const std::chrono::steady_clock::time_point &start_time,
+                       const std::chrono::steady_clock::time_point &end_time, size_t num_packages)
+{
+    double duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count() / 1000.0;
+    double trans_data_size = cdata_size * num_packages / 10e6;
+    std::cout << "Transfered " << trans_data_size << "MBytes in " << duration << "s, Transfer rate  " << trans_data_size / duration;
+    std::cout << "MByte/s, " << trans_data_size / duration * 8 << "MBit/s" << std::endl;
+    std::cout << "Number of cpu samples :" << cpu_samples.size() << std::endl;
+    long double cpu_average = accumulate(cpu_samples.begin(), cpu_samples.end(), 0) / cpu_samples.size();
+    std::cout << "Average CPU load: " << cpu_average << "%" << std::endl;
+}
+
 void sample_cpu_load()
 {
     init_cpu();
+    gMeasure = true;
     while(gMeasure)
     {
         cpu_samples.push_back(getCurrentValue());
@@ -77,14 +92,17 @@ void dummy_free(void *data, void *hint){};
 
 int main() {
 
-    zmq::context_t context (1);
-    zmq::socket_t socket (context, ZMQ_REP);
-    socket.bind ("tcp://*:5555");
+
     const size_t cdata_size = 4000*3000;
     std::shared_ptr<char> data(new char[cdata_size]);
     std::shared_ptr<std::thread> cpu_thread;
     std::chrono::steady_clock::time_point start_time, end_time;
     size_t num_packages=0;
+
+    // ZEROMQ
+    zmq::context_t context (1);
+    zmq::socket_t socket (context, ZMQ_REP);
+    socket.bind ("tcp://*:5555");
     while (true) {
         zmq::message_t request;
 
@@ -109,15 +127,42 @@ int main() {
         socket.send (reply);
         num_packages++;
     }
+
     std::cout << "=== ZEROMq ===" << std::endl;
-    double duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time-start_time).count() / 1000.0;
-    double trans_data_size = cdata_size * num_packages / 10e6;
-    std::cout << "Transfered " << trans_data_size << "MBytes in "  << duration << "s, Transfer rate  " << trans_data_size / duration;
-    std::cout << "MByte/s, " << trans_data_size / duration * 8<<"MBit/s" << std::endl;
-    std::cout << "Number of cpu samples :" << cpu_samples.size() << std::endl;
-    long double cpu_average = std::accumulate(cpu_samples.begin(), cpu_samples.end(), 0) / cpu_samples.size();
-    std::cout << "Average CPU load: " << cpu_average << "%" << std::endl;
+    report_statistics(cdata_size, start_time, end_time, num_packages);
+
+    // NANOMSG
+    int sock = nn_socket (AF_SP, NN_PAIR);
+    num_packages = 0;
+    nn_bind (sock, "tcp://*:5556");
+    cpu_thread = NULL;
+    while (true) {
+
+        char *buf = NULL;
+        nn_recv (sock, &buf, NN_MSG, 0);
+        if(cpu_thread == NULL)
+        {
+            cpu_thread = std::shared_ptr<std::thread>(new std::thread(sample_cpu_load));
+            start_time = std::chrono::steady_clock::now();
+        }
+        if(buf[0] == ' ')
+        {
+            gMeasure = false;
+            cpu_thread->join();
+            end_time = std::chrono::steady_clock::now();
+            break;
+        }
+        //  Send
+        nn_freemsg (buf);
+        nn_send (sock, data.get(), cdata_size, 0);
+        num_packages++;
+    }
+
+    std::cout << "=== NANOMSG ===" << std::endl;
+    report_statistics(cdata_size, start_time, end_time, num_packages);
+
 
 
     return 0;
 }
+
